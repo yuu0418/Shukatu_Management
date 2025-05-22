5b8c3afb26348c87d683bd1bd848d4738be16b98
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-05-22 13:47:26 +0900

    feat: root-page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,17 @@
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import TableView from "@/components/TableView";
+
 export default function Home() {
   return (
-   <div>home</div>
+    <div className="flex h-screen flex-col bg-white">
+      <div className="flex h-full overflow-hidden">
+        <Sidebar />
+        <div className="flex flex-1 flex-col overflow-hidden">
+          <Header />
+          <TableView />
+        </div>
+      </div>
+    </div>
   );
 }
